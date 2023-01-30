728b69e127580005ee46b65c2aa463aecd4de060
Tanish25 <tanishht@gmail.com>
2023-01-30 13:42:35 +0530

    new commit

--- UART/Interrupt_MultiModule/main.c
+++ UART/Interrupt_MultiModule/main.c
@@ -4,70 +4,72 @@
 #include <string.h>
 
 volatile char recv_character;
-//char transmit_ch = ' ';
-
+volatile uint8_t recv_byte;
 //char send_str[]="AT+CWJAP=\"AndroidAP\",\"tan12345\"\r\n";
-char send_str[] = "AT\r\n";
-//char send_str[]="AT+CWMODE=1\r\n";
+char send_str[]="AT\r\n";
+//char send_str[] = "AT+CWMODE=1\r\n";
+char recv_str[];
 volatile uint8_t len;
 volatile uint8_t i = 0;
+volatile uint8_t j = 0;
 
 void uart1_init(void)
- {
+{
   // configure the first UART module for transmitting data initially
-  UBRR1 = 8;                                                                             // for 115200 baud rate with F_CPU = 16000000
-  UCSR1B |= (1 << TXEN1)|(1 << UDRIE1);  // enable receiver and receive complete interrupt
+  UBRR1H = 0;  
+  UBRR1L = 8; // for 115200 baud rate with F_CPU = 16000000
+  UCSR1B |= (1 << TXEN1)|(1 << UDRIE1);  // enable transmitter and UDRE interrupt
   UCSR1A = 0x00;
   UCSR1C = 0x06;  //Transmission size = 8 bit; asynchronous transmission
 }
 
 void uart2_init(void) 
 {
-  // configure the second UART module for transmitting the received data
-  UBRR2 = 8;               // for 115200 baud rate with F_CPU = 16000000
-  UCSR2B |= (1 << TXEN2);
-   UCSR2A = 0x00;
+  // configure the second UART module for receiving data
+  UBRR2H = 0;  
+  UBRR2L = 8;              // for 115200 baud rate with F_CPU = 16000000
+  UCSR2B |= (1 << RXEN2)| (1 << RXCIE2);
+  UCSR2A = 0x00;
   UCSR2C = 0x06;  
 }
 
 void uart3_init(void) 
 {
-  // configure the third UART module for receiving data
-  UBRR3 = 8;               // for 115200 baud rate with F_CPU = 16000000
-  UCSR3B |= (1 << RXEN3)| (1 << RXCIE3);
-   UCSR3A = 0x00;
+	 // configure the third UART module for transmitting the received data
+  
+  UBRR3H = 0;  
+  UBRR3L = 8;               // for 115200 baud rate with F_CPU = 16000000
+  UCSR3B |=(1 << RXEN3);
+  UCSR3A = 0x00;
   UCSR3C = 0x06;  
 }
 
+
 ISR(USART1_UDRE_vect)
- {
-  //lenr=len_calc(send_str);
-
+{
   if (i < len) 
-  	{ 
+  { 
   	UDR1 = send_str[i++];
-  	//UCSR1B |= (1 << UDRIE1); 
-   }
+  }
 }
 
-ISR(USART3_RX_vect) {
-  recv_character = UDR3;    // read the received data
-  UCSR2B |= (1 << UDRIE2);  // enable the data register empty interrupt for the second UART module
+ISR(USART2_RX_vect) 
+{
+  recv_byte = UDR2;
+  recv_str[j++] = recv_byte; 
+  UDR3 = recv_byte;  
 }
 
-ISR(USART2_UDRE_vect) {
-  UDR2 = recv_character;     // send the received data through the second UART module
-  UCSR2B &= ~(1 << UDRIE2);  // disable the data register empty interrupt for the second UART module
-}
 
-int main(void) {
-
+int main(void) 
+{
+	
   uart1_init();
   uart2_init();
   uart3_init();
   len=strlen(send_str);
   sei();  // enable global interrupts
-
-  while (1);
+  while(1);
   return 0;
 }
+
--- UART/UDREN0_String_Send/main.c
+++ UART/UDREN0_String_Send/main.c
@@ -6,8 +6,8 @@
 
 #define BAUDRATE  115200L
 uint8_t recv_byte;
-char recv_str[]="";
-char recv_character=' ';
+char recv_str[];
+char recv_character;
 		
 
 
@@ -56,124 +56,36 @@ void uart_recv(void)
 {
 	while(1)
 	{
-	while(!(UCSR0A & (1<<RXC0))); //wait until character is received
-	recv_byte = UDR0;
-	PORTL=0XFF;
-	//_delay_us(182);
-	UDR2 = recv_byte;
-	while(!(UCSR2A & (1<<UDRE2)));
+		while(!(UCSR0A & (1<<RXC0))); //wait until character is received
+		recv_byte = UDR0;
+		PORTL=0XFF;
+		//_delay_us(182);
+		UDR2 = recv_byte;
+		while(!(UCSR2A & (1<<UDRE2)));
 	//recv_character = (char) (recv_byte);
 	//strncat(recv_str, &recv_character, 1);
 	}
 
 }
-/*
-void uart_send_default(void)
-{
-	UDR0 = 0x0D;
-	while(!(UCSR0A & (1<<UDRE0)));
-	//_delay_us(90);//delay configured such that maybe processing time is to be accounted for
-	UDR0 = 0x0A;
-	while(!(UCSR0A & (1<<UDRE0)));
-	
-}
-*/
+
 int main(void)
 {
-	
-	
-		//DDRC = 0xFF;//PORTC for output
-		//DDRA = 0xFF;//PORTA for output
-	DDRL = 0xFF;//PORTL for output
 
-	//DDRE=0x02; // SET PE1 / TX0 as output
-	//DDRD=0x00;// SET PD as input (we need PD2 = RX1)
-
-	PORTL = 0x00;
-	//_delay_us(100);
 	uart_init();
-/*	
-
-	uart_send(0x0d);
-	_delay_us(20);
-	//uart_send(0x0a);
-
-
-	UCSR0A &= 0xBF;	
-	PORTL=0x00;
-	uart_init();
-	UDR0 = 0x0a;
-	UCSR0A &= 0xBF;
-	_delay_us(1);
-	UDR0 = 0x7F;
-	_delay_us(1);
-	UDR0 = 0x77;
-
-
-	while(1)
-	{
-			if (UCSR0A & 0x20)
-				PORTL=0xff;
-			else
-				PORTL = 0x00;
-
-
-	}
-
-
-	while(1);
-
-	//UCSR1A &= 0x7F;
-
-
-
-	
-
-	UCSR0A &= 0xBF;
-	UDR0 = 0x0D;
-
-	while(1)
-	{
-			if (UCSR0A & 0x40)
-				PORTL=0xff;
-			else
-				PORTL = 0x00;
-
-
-	}
-
-*/
 	//char send_str[]="AT+CWJAP=\"AndroidAP\",\"tan12345\"";
 	//char send_str[]="AT\r\n";
 	char send_str[]="AT+CWMODE=1\r\n";
 	uint8_t len=strlen(send_str);
 	uint8_t i=0;
-	
 
-	
-		//uart_send('U');
-		//_delay_us(1000);
-
-		//Wait for char on RX1
-		
-		//uart_send(recv_data);
 		
 	while(i<len)
 	{
 		uart_send(send_str[i++]);
 	}
 	i=0;
-	
-	//uart_send_default();
-	
-	
-	   
-		//_delay_us(250);
 	uart_recv();
-		//uart_send('U');
 
-
-	
     return 0;
 }
 //PORT A= 22-29; PORT C = 37-30; PORTL= 42-49
